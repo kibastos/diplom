4dd8f02d84e66f47bd8d242993b27be77581caf4
kibastos <130778507+kibastos@users.noreply.github.com>
2023-06-22 06:23:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,19 +16,43 @@ $(function () {
   // })
 
 
-  $('.js-tab-trigger').click(function() {
+
+  $('.js-tab-trigger').click(function () {
     var id = $(this).attr('data-tab'),
-        content = $('.js-tab-content[data-tab="'+ id +'"]');
-    
+      content = $('.js-tab-content[data-tab="' + id + '"]');
     $('.js-tab-trigger.active').removeClass('active');
     $(this).addClass('active');
     
     $('.js-tab-content.active').removeClass('active');
     content.addClass('active');
+
+  });
+
+  $(".js-tab-trigger").click(function(){
+    $('active').show(2000);
 });
 
 
+$('.before__slider').slick({
+  arrows: false,
+  dots: true,
+  draggable: false,
+  appendDots: $('.before__slide-dots')
+})
+$('.before__prev').on('click', function (e) {
+  e.preventDefault()
+  $('.before__slider').slick('slickPrev')
+})
+$('.before__next').on('click', function (e) {
+  e.preventDefault()
+  $('.before__slider').slick('slickNext')
+})
 
 
 })
+
+function beforeAfter() {  
+  document.getElementById('kobavenusab').style.width = document.getElementById('pedsumid').value + "%";
+}
+
 
