f2a2180f2dfa388511b04e7582b999c5b988606c
kibastos <130778507+kibastos@users.noreply.github.com>
2023-06-26 04:24:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,9 +28,7 @@ $(function () {
 
   });
 
-  $(".js-tab-trigger").click(function(){
-    $('active').show(2000);
-});
+
 
 
 $('.before__slider').slick({
@@ -47,6 +45,36 @@ $('.before__next').on('click', function (e) {
   e.preventDefault()
   $('.before__slider').slick('slickNext')
 })
+
+
+$('.cert__slider').slick({
+  arrows: false,
+  infinite: true,
+  slidesToShow: 3,
+})
+$('.cert__prev').on('click', function (e) {
+  e.preventDefault()
+  $('.cert__slider').slick('slickPrev')
+})
+$('.cert__next').on('click', function (e) {
+  e.preventDefault()
+  $('.cert__slider').slick('slickNext')
+})
+
+$('.faq__acc-link').on('click', function (e) {
+  e.preventDefault()
+  if ($(this).hasClass('faq__acc-link--active')) {
+    $(this).removeClass('faq__acc-link--active')
+    $(this).children('.faq__acc-text').slideUp()
+  } else {
+    $('.faq__acc-link').removeClass('faq__acc-link--active')
+    $('.faq__acc-text').slideUp()
+    $(this).addClass('faq__acc-link--active')
+    $(this).children('.faq__acc-text').slideDown()
+
+  }
+})
+
 
 
 })
